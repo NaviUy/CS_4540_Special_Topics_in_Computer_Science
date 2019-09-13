cadbdc0e355daccbdebf8add56ef97fb23179491
Ivan Yu <ivanscreativity@gmail.com>
2019-09-13 02:50:49 -0700

    attempt at delegation

--- src/homework1/Subscriber.kt
+++ src/homework1/Subscriber.kt
@@ -1,6 +1,7 @@
 package homework1
 
-class Subscriber : Observer {
+class Subscriber(s : Subject) : Observer, Subject by s {
+
     override fun update(newUpdate: String) {
         println("Look new update: $newUpdate")
     }
